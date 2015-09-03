3861b2290d8d670b581454f263661bf701e5d4de
Gerhard Pegel <gpegel@gmail.com>
2015-09-03 22:59:07 +0200

    My first C# console app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a comment
+            // this WriteLine is a method of the Class called Console
+            Console.WriteLine("Hello World, this is my first C# app");
+            // this ReadLine is a method of the Class called Console also
+            Console.ReadLine();
+        }
+    }
+}
